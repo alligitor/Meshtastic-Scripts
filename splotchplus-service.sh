f84ae34af5118b516ef00c67797215ddba2ef6e9
alligitor <8754766+alligitor@users.noreply.github.com>
2025-04-26 15:54:20 -0400

    kMoved the top level directory, had to change the script

--- splotchplus-service.sh
+++ splotchplus-service.sh
@@ -2,6 +2,6 @@
 # setup the python env
 source ~/meshtastic/bin/activate
 # make sure we are in the correct directory
-cd /home/pi/Meshtastic-Scripts
+cd /home/pi/Alligitor/Meshtastic-Scripts
 /usr/bin/screen -S splotchplus -d -m /home/pi/meshtastic/bin/python3 BotTastic.py
 
