bb3085594664793a82976ede418c74d91c4ee32e
alligitor <8754766+alligitor@users.noreply.github.com>
2025-05-07 17:26:21 -0400

    Fixed a variable name issue.

--- BotTastic.py
+++ BotTastic.py
@@ -193,7 +193,7 @@ def loadDataFromJSONFile(filename):
             data = json.load(f, object_hook=object_hook)
             return data
     except FileNotFoundError:
-        print(f"File {file_path} not found.")
+        print(f"File {filename} not found.")
         return None
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
